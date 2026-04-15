f2c4e8f3cf1cd47cac2123dfe0b8af719d71cb8f
Mohammad Fayaz <35001172+fayaz07@users.noreply.github.com>
2026-04-15 10:27:17 +0530

    add github action for deploy

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  reactCompiler: true,
+  basePath: "/cafe-loaded",
+  assetPrefix: "/cafe-loaded",
 };
 
 export default nextConfig;
